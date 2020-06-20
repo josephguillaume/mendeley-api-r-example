ec957afb1981d228456577b2495beb8048edd2a5
Joseph Guillaume <josephguillaume@gmail.com>
2020-06-20 17:03:21 +1000

    Modify example to use OAuth Authorization code flow with httr functions

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
-# Mendeley API R Example #
+# Mendeley API R OAuth Authorization code Example #
 
-This is a simple example of an application that consumes the [Mendeley](http://www.mendeley.com) API. It uses the OAuth client credentials flow. For more information on the API, see the [developer portal](http://dev.mendeley.com).
+This is a simple example of an application that consumes the [Mendeley](http://www.mendeley.com) API. It is modified from https://github.com/Mendeley/mendeley-api-r-example to use the OAuth Authorization code flow. For more information on the API, see the [developer portal](http://dev.mendeley.com).
 
 ## About the application ##
 
-The application is a simple command-line script that retrieves the number of Mendeley users that have read a document with a given DOI, and produces a barplot of the readers by academic status.
+The application is a script that searches for a document by DOI, retrieves the number of Mendeley users that have read the document, and produces a barplot of the readers by academic status.
 
 ## Prerequisites ##
 
-Register your client at the [developer portal](http://dev.mendeley.com).  This will give you a client ID and secret. Save them somewhere safe - they are sensitive credentials, and once the secret is set it is never revealed again (to protect it).
+Register your client at the [developer portal](http://dev.mendeley.com).  This will give you a client ID and secret. Use the Redirect URL http://localhost:1410/
 
 ## How to run ##
 
 1. Install [R](http://www.r-project.org/).
-2. In a shell, or in Rstudio, set the MENDELEY_CLIENT_ID and MENDELEY_CLIENT_SECRET environment variables to the values you just got from the developer portal.
-3. Install the dependencies declared at the top of the script.
-4. Think of a DOI you would like to search for.
-5. Run the script, passing the DOI in as a command line argument. You can do that like this:
+2. Install the dependencies declared at the top of the script.
+3. Set the options for your client, e.g. `options("MENDELEY_CLIENT_ID"=8362,"MENDELEY_CLIENT_SECRET"="O0iIfk3S7xaU4jlf")`
+4. Run the script
 
-        Rscript mendeley.R --doi 10.1.1.105.1540
-
-
-## Build status
-
-![Travis CI](https://travis-ci.org/Mendeley/mendeley-api-r-example.svg?branch=master)
--- mendeley.R
+++ mendeley.R
@@ -1,55 +1,47 @@
 require(httr)
-require(rjson)
-require(RCurl)
-require(RColorBrewer)
-require(optparse)
 
-option_list <- list(
-  make_option(c("-d", "--doi"), action="store_true", help="DOI to search for")
-)
-program_args = parse_args(OptionParser(option_list = option_list))
-doi <- program_args$doi
+# Obtain OAuth client ID and secret from the corresponding environment variables
+# Register at https://dev.mendeley.com/myapps.html with Redirect URL http://localhost:1410/
 
-message("Obtain OAuth client ID and secret from the corresponding environment variables")
+client_id <- getOption("MENDELEY_CLIENT_ID")
+client_secret <- getOption("MENDELEY_CLIENT_SECRET")
 
-client_id <- Sys.getenv("MENDELEY_CLIENT_ID")
-client_secret <- Sys.getenv("MENDELEY_CLIENT_SECRET")
-basic_auth_credentials = RCurl::base64(paste(client_id, client_secret, sep = ":"))
+if(is.null(client_id)) stop('options("MENDELEY_CLIENT_ID") not set')
+if(is.null(client_secret)) stop('options("MENDELEY_CLIENT_SECRET") not set')
 
-message("Obtain OAuth access token")
+# Obtain OAuth access token
 
-access_token_rsp = POST('https://api.mendeley.com/oauth/token',
-	   add_headers(
-		       'Authorization' = paste("Basic", basic_auth_credentials),
-		       'Content-Type' = 'application/x-www-form-urlencoded'
-	   ),
-	   body = 'grant_type=client_credentials&scope=all'
-)
+endpoint=oauth_endpoint(NULL,
+                        authorize="authorize",
+                        access="token",
+                        base_url = "https://api.mendeley.com/oauth")
+app=oauth_app("mendeley",client_id,client_secret)
+token=oauth2.0_token(endpoint,app,scope="all")
 
-access_token = content(access_token_rsp)$access_token
+# Search the Mendeley catalogue for the specified DOI
 
-message("Search the Mendeley catalogue for the specified DOI")
+doi = '10.1016/j.molcel.2009.09.013'
 
-catalogue_url = paste('https://api.mendeley.com/catalog?view=stats&doi=', curlEscape(doi), sep='')
+catalogue_url = modify_url('https://api.mendeley.com/catalog?view=stats', 
+                           query=list(doi=doi))
 doc_rsp <- GET(catalogue_url,
-	       add_headers(
-			   'Authorization' = paste("Bearer", access_token),
-			   'Content-Type' = 'application/vnd.mendeley-document.1+json'
-	       )
+               config(token = token),
+               accept('application/vnd.mendeley-document.1+json')
 )
 
-docs <- fromJSON(rawToChar(content(doc_rsp)))
+docs <- content(doc_rsp,as = "parsed",type="application/json")
 
 if (length(docs) > 0) {
   doc <- docs[[1]]
   message(paste(doc$title, 'has', doc$reader_count, 'readers.'))
   
-  message("Create visual representation of the data")
+  # Create visual representation of the data
 
   df <- data.frame(t(data.frame(doc$reader_count_by_academic_status)))
   colnames(df) <- c('readers')
   par(mar=c(5,18,1,1), las=2)
-  barplot(df$readers, names.arg=row.names(df), horiz=TRUE, col=brewer.pal(n=15, name="Reds"))
+  barplot(df$readers, names.arg=row.names(df), horiz=TRUE)
 } else {
   message('Document not found.')
 }
+
